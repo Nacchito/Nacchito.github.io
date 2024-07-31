7036d2f1451e4d184f24854ba119ab1d9b1bbb7a
Nacchito <nachodaniel41@gmail.com>
2024-07-30 22:17:56 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function cambiamensaje() {
+document.getElementById("loading-message").innerText="Enviado!"
+}
+
+setTimeout(cambiamensaje,2300)
+
+setTimeout(function () {
+    alert("Recibiste un abrazo de tu novio!")
+}, 2400)
